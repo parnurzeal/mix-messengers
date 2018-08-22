e8c6ef82755f9c72a7384d9c6a1394ca504d0f61
parnurzeal <parnurzeal@gmail.com>
2018-08-22 22:26:51 +0800

    Init FB and try get test page data.

--- src/main.js
+++ src/main.js
@@ -8,6 +8,37 @@ import store from './store';
 Vue.config.productionTip = false;
 
 function created() {
+  /* eslint-disable */
+  window.fbAsyncInit = function () {
+    FB.init({
+      appId: '266788757017683',
+      autoLogAppEvents: true,
+      xfbml: true,
+      version: 'v3.1',
+    });
+    FB.api(
+      '/602939823441479',
+        'GET',
+        {"fields":"conversations{id}"},
+        function(response) {
+          console.log(response);
+                // Insert your code here
+        }
+    );
+    FB.getLoginStatus(function(response) {
+      console.log(response);
+    });
+  };
+
+  (function (d, s, id) {
+    let js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+  /* eslint-enable */
   console.log('hello world');
   const me = {
     uuid: 'createdUuid',
